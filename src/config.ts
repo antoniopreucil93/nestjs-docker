250bf115195d23bdd1e4faa88e81b1d7009cba84
Antonio Preucuil <antonio.preucil@protonmail.com>
2020-10-18 17:46:09 +0200

    env variables added to api container

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const pgConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: `db`,
-  port: 5432,
-  username: `postgres`,
-  password: `postgres`,
-  database: `nest_docker`,
+  host: `${process.env.POSTGRES_HOST}`,
+  port: +`${process.env.POSTGRES_PORT}`,
+  username: `${process.env.POSTGRES_USER}`,
+  password: `${process.env.POSTGRES_PASSWORD}`,
+  database: `${process.env.POSTGRES_DB}`,
   entities: ['dist/**/*.entity.js'],
   synchronize: true,
   logger: 'debug',
